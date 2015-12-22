2c91d1a44fd1e9dd757a41973efdf5ca4b3104b4
Court Fowler <courtf@gmail.com>
2015-12-22 10:49:08 -0800

    rest of the web mercator helpers

--- web.go
+++ web.go
@@ -34,9 +34,29 @@ func (w World) MercToPixel(x, y float64, zoom uint, tileSize int) (px, py float6
 	return
 }
 
+func (w World) PixelToMerc(px, py float64, zoom uint, tileSize int) (x, y float64) {
+	// move origin to bottom left
+	py = float64(w.PixelCircumference(zoom, tileSize)) - py
+
+	metersPerPixel := w.Resolution(zoom, tileSize)
+	shift := w.Circumference / 2
+
+	x = (px * metersPerPixel) - shift
+	y = (py * metersPerPixel) - shift
+	return
+}
+
 func (w World) MercToTile(x, y float64, zoom uint, tileSize int) (tx, ty int) {
 	px, py := w.MercToPixel(x, y, zoom, tileSize)
 	tx, ty = w.PixelToTile(px, py, tileSize)
+	return
+}
+
+// because tiles are coarse, accuracy is lost going from tile coords -> other coords
+// the mercator coords returned are those of the top left corner of the tile
+func (w World) TileToMerc(tx, ty int, zoom uint, tileSize int) (x, y float64) {
+	px, py := w.TileToPixel(tx, ty, tileSize)
+	x, y = w.PixelToMerc(px, py, zoom, tileSize)
 	return
 }
 
@@ -46,12 +66,32 @@ func (w World) PixelToTile(px, py float64, tileSize int) (tx, ty int) {
 	return
 }
 
+// because tiles are coarse, accuracy is lost going from tile coords -> other coords
+// the pixel coords returned are those of the top left corner of the tile
+func (w World) TileToPixel(tx, ty int, tileSize int) (px, py float64) {
+	px = float64(tx * tileSize)
+	py = float64(ty * tileSize)
+	return
+}
+
 func (w World) LonLatToPixel(lon, lat float64, zoom uint, tileSize int) (px, py float64) {
 	x, y := w.LonLatToMerc(lon, lat)
 	return w.MercToPixel(x, y, zoom, tileSize)
 }
 
+func (w World) PixelToLonLat(px, py float64, zoom uint, tileSize int) (lon, lat float64) {
+	x, y := w.PixelToMerc(px, py, zoom, tileSize)
+	return w.MercToLonLat(x, y)
+}
+
 func (w World) LonLatToTile(lon, lat float64, zoom uint, tileSize int) (tx, ty int) {
 	px, py := w.LonLatToPixel(lon, lat, zoom, tileSize)
 	return w.PixelToTile(px, py, tileSize)
+}
+
+// because tiles are coarse, accuracy is lost going from tile coords -> other coords
+// the lat & lon returned are those of the top left corner of the tile
+func (w World) TileToLonLat(tx, ty int, zoom uint, tileSize int) (lon, lat float64) {
+	x, y := w.TileToMerc(tx, ty, zoom, tileSize)
+	return w.MercToLonLat(x, y)
 }
