5660c03f31f2d89c856e876acadcaec162ab1deb
Court Fowler <courtf@gmail.com>
2015-12-21 23:24:24 -0800

    move to osm/google coordinates instead of tms

--- web.go
+++ web.go
@@ -6,24 +6,38 @@ import "math"
 
 // Thanks: http://www.maptiler.org/google-maps-coordinates-tile-bounds-projection/
 
+// funcs here produce coordinates with the origin in the top letf, so pixel and tile
+// coordinates are in OSM/Google format, not TMS.
+
+func (w World) PixelCircumference(zoom uint, tileSize int) int {
+	return tileSize * (1 << zoom)
+}
+
 // meters per pixel, at the provided zoom and tile size
 func (w World) Resolution(zoom uint, tileSize int) float64 {
-	return w.Circumference / float64(tileSize*(1<<zoom))
+	return w.Circumference / float64(w.PixelCircumference(zoom, tileSize))
 }
 
 func (w World) MercToPixels(x, y float64, zoom uint, tileSize int) (px, py float64) {
 	metersPerPixel := w.Resolution(zoom, tileSize)
+
+	// this shift moves the cordinate system to [0, Circumference]
+	// in both dimensions
 	shift := w.Circumference / 2
 
 	px = (x + shift) / metersPerPixel
 	py = (y + shift) / metersPerPixel
+
+	// move origin to top left
+	py = float64(w.PixelCircumference(zoom, tileSize)) - py
+
 	return
 }
 
 func (w World) MercToTile(x, y float64, zoom uint, tileSize int) (tx, ty int) {
 	px, py := w.MercToPixels(x, y, zoom, tileSize)
 
-	tx = int(math.Ceil(px/float64(tileSize)) - 1)
-	ty = int(math.Ceil(py/float64(tileSize)) - 1)
+	tx = int(math.Floor(px / float64(tileSize)))
+	ty = int(math.Floor(py / float64(tileSize)))
 	return
 }
